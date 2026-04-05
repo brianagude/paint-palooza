beafcab76d05e70488dff9a7223974480fcd4cb7
Briana Gude <briana.gude@gmail.com>
2026-04-05 12:59:07 -0400

    fix css build issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	css: {
+		transformer: "postcss",
+	},
+	build: {
+		cssMinify: "esbuild",
+	},
+});
