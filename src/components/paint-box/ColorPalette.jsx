7ce9f91586aa0c073b01f8686f81bdeb557fa96c
Briana Gude <briana.gude@gmail.com>
2026-05-07 10:34:32 -0400

    updated the paintbox context

--- src/components/paint-box/ColorPalette.jsx
+++ src/components/paint-box/ColorPalette.jsx
@@ -1,7 +1,9 @@
 import { ryb2rgb } from 'rybitten';
 import { cubes } from 'rybitten/cubes';
+import { usePaint } from "./PaintBox"
 
-export function ColorPalette({cubeKey, onSelectSwatch, currentColor}) {
+export function ColorPalette() {
+  const { colorSpace, foregroundColor, setForegroundColor, backgroundColor, setBackgroundColor } = usePaint();
   const PALETTE_RYB = [
     [0, 0, 0],
     [1, 0, 0],
@@ -34,27 +36,30 @@ export function ColorPalette({cubeKey, onSelectSwatch, currentColor}) {
   ];
 
   function rybToRgbString([r, y, b]) {
-    const [cr, cg, cb] = ryb2rgb([r, y, b], { cube: cubes.get(cubeKey).cube });
+    const [cr, cg, cb] = ryb2rgb([r, y, b], { cube: cubes.get(colorSpace).cube });
     return `rgb(${Math.round(cr * 255)}, ${Math.round(cg * 255)}, ${Math.round(cb * 255)})`;
   }
-  const currentBg = rybToRgbString(currentColor);
 
-  return <div className="color-palette">
-    <div className="current-swatches">
-      <button type="button" className="swatch-btn selected" style={{ background: currentBg }}/>
+  const currentBg = rybToRgbString(backgroundColor);
+
+  return (
+    <div className="color-palette">
+      <div className="current-swatches">
+        <button type="button" className="swatch-btn selected" style={{ background: currentBg }}/>
+      </div>
+      <div className="swatch-rows">
+        {PALETTE_RYB.map((c, i) => {
+          return (
+            <button
+              key={`button-${c[0]}-${c[1]}-${c[2]}-${i}`}
+              type="button"
+              className="swatch-btn selected"
+              style={{ background: rybToRgbString(c) }}
+              onClick={() => setForegroundColor(c)}
+            />
+          );
+        })}
+      </div>
     </div>
-    <div className="swatch-rows">
-      {PALETTE_RYB.map((c, i) => {
-        return (
-          <button
-            key={`button-${c[0]}-${c[1]}-${c[2]}-${i}`}
-            type="button"
-            className="swatch-btn selected"
-            style={{ background: rybToRgbString(c) }}
-            onClick={() => onSelectSwatch(c)}
-          />
-        );
-      })}
-    </div>
-  </div>
+  )
 }
