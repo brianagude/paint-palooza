89fa432c033ed06c1afb716e691185df93ea62b5
Briana Gude <briana.gude@gmail.com>
2026-06-15 11:18:45 -0400

    small update

--- src/components/Dektop.jsx
+++ src/components/Dektop.jsx
@@ -1,32 +1,24 @@
-import {useOS} from "./OS"
 import arrow from "@/assets/icons/app-arrow.jpg";
+import { useOS } from "./OS";
 import "../App.css";
 
 export function Desktop() {
-  const {registeredApps, launch} = useOS();
-  const elements = [];
-  registeredApps.forEach((app, id) => {
-    elements.push(
-      <button
-        type="button"
-        className="app-btn"
-        onDoubleClick={() => launch(id)}
-      >
-        <div>
-          <img
-            src={app.icon}
-            alt={app.name + ' Icon'}
-            className="app-icon"
-          />
-          <img src={arrow} alt="Arrow Icon" className="app-icon-arrow" />
-        </div>
-        <p className="app-title">{app.name}</p>
-      </button>
-    );
-  });
-  return (
-    <div className="app-icons">
-      {elements}
-    </div>
-  )
+	const { registeredApps, launch } = useOS();
+	const elements = [];
+	registeredApps.forEach((app, id) => {
+		elements.push(
+			<button
+				type="button"
+				className="app-btn"
+				onDoubleClick={() => launch(id)}
+			>
+				<div>
+					<img src={app.icon} alt={`${app.name} Icon`} className="app-icon" />
+					<img src={arrow} alt="Arrow Icon" className="app-icon-arrow" />
+				</div>
+				<p className="app-title">{app.name}</p>
+			</button>,
+		);
+	});
+	return <div className="app-icons">{elements}</div>;
 }
